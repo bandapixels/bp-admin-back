059e2c6b28e13960fb4cc45f85d23d1d52af18a3
VladislavBrykov <vlad5brykov@gmail.com>
2021-10-18 16:48:53 +0300

    refactor/TASK-21/work with conflict

--- src/admin/post/schema/create.post.schema.ts
+++ src/admin/post/schema/create.post.schema.ts
@@ -4,10 +4,7 @@ export const CreatePostSchema = Joi.object({
   head: Joi.string().required().max(255),
   subtitle: Joi.string().max(255),
   excerpt: Joi.string().max(2048).required(),
-  image: Joi.any().required(),
-  preview_image: Joi.any().required(),
   tags: Joi.array().items(Joi.string()),
   mins_to_read: Joi.number().min(0).required(),
   body: Joi.string().max(4294967294).required(),
-  // files: Joi.any(),
 });
