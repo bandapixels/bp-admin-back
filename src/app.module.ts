5d661df48e0d8d3353f783522b375d15baac044f
Viktoriia Bob <vikabobir32@gmail.com>
2021-10-19 15:48:22 +0300

    TASK-27/Generate-migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,18 @@
-import {
-  forwardRef,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/auth/Models/users.module';
 import { UserController } from 'src/auth/Controllers/user.controller';
 import { UserService } from 'src/auth/Service/User/user.service';
-import { LoggerMiddleware } from 'src/auth/Middlewares/logger.middleware';
-import { User } from 'src/auth/entity/User';
 import * as dotenv from 'dotenv';
-import { Tag } from 'src/admin/tag/entity/admin.tag.entity';
 import AdminTagModule from 'src/admin/tag/admin.tag.module';
+
 dotenv.config({});
-import { Post } from './admin/post/entities/admin.post.entity';
-import AdminPostModule from "src/admin/post/admin.post.module";
+import AdminPostModule from 'src/admin/post/admin.post.module';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DATABASE,
-      entities: [User, Tag, Post],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     AdminTagModule,
     AdminPostModule,
   ],
