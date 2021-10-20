9076ba06426976b34aca5c20cd322de16f341d69
Viktoriia Bob <vikabobir32@gmail.com>
2021-10-20 09:21:30 +0300

    TASK-7/Post-crud

--- src/admin/post/admin.post.controller.ts
+++ src/admin/post/admin.post.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  Param,
   Post,
   Query,
   Render,
@@ -15,6 +16,7 @@ import { JoiValidationPipe } from '../../filter/joi.validation.pipe';
 import { CreatePostSchema } from './schema/create.post.schema';
 import { Roles } from '../../auth/decorators/roles.decorator';
 import { Role } from '../../auth/Models/role.enum';
+import { UpdatePostSchema } from './schema/update.post.schema';
 
 @Controller('admin/posts')
 @Roles(Role.ADMIN)
@@ -43,5 +45,35 @@ export class AdminPostController {
   public async createPost(@Body() newPost: PostDto, @Res() res) {
     await this.adminPostService.createPost(newPost);
     res.redirect('/admin/posts');
+  }
+
+  @Get('/:id/edit')
+  @Render('layouts/app.ejs')
+  public async editingPost(@Param('id') id) {
+    const post = await this.adminPostService.getPostById(id);
+    const tags = await this.adminTagService.getAllTags();
+    return { post, tags, body: '../admin/post/edit.ejs', isAuthorized: true };
+  }
+
+  @Post('/:id/edit')
+  @UsePipes(new JoiValidationPipe(UpdatePostSchema))
+  public async editPost(
+    @Body() editPost: PostDto,
+    @Param('id') id,
+    @Res() res,
+  ) {
+    await this.adminPostService.updatePost(id, editPost);
+    res.redirect('/admin/posts');
+  }
+
+  @Get('/:id/publish')
+  public async changePublish(@Param('id') id, @Res() res) {
+    await this.adminPostService.changePublishValue(id);
+    res.redirect('/admin/posts');
+  }
+
+  @Post('/:id/delete')
+  public async deletePost(@Param('id') id) {
+    return this.adminPostService.deletePost(id);
   }
 }
---
+++ src/admin/post/schema/update.post.schema.ts
@@ -0,0 +1,13 @@
+import * as Joi from 'joi';
+
+export const UpdatePostSchema = Joi.object({
+  head: Joi.string().required().max(255),
+  subtitle: Joi.string().max(255).allow('', null),
+  image: Joi.any().optional(),
+  preview_image: Joi.any().optional(),
+  excerpt: Joi.string().max(2048).required(),
+  tags: Joi.array().items(Joi.string()),
+  mins_to_read: Joi.number().min(0).required(),
+  body: Joi.string().max(4294967294).required(),
+  files: Joi.any().optional(),
+});
