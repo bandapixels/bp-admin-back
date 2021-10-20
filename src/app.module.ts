a5a6d2bf3076e9a4fad08b4b5155f6f2252ff7ed
VladislavBrykov <vlad5brykov@gmail.com>
2021-10-20 10:35:39 +0300

    refactor dev/hash password, joi validator, new constants

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,3 @@
-// import { forwardRef, Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { UsersModule } from 'src/auth/Models/users.module';
-// import { UserController } from 'src/auth/Controllers/user.controller';
-// import { UserService } from 'src/auth/Service/User/user.service';
-// import * as dotenv from 'dotenv';
-// import AdminTagModule from 'src/admin/tag/admin.tag.module';
-//
-// dotenv.config({});
-// import AdminPostModule from 'src/admin/post/admin.post.module';
-//
-// @Module({
-//   imports: [
-//     forwardRef(() => UsersModule),
-//     TypeOrmModule.forRoot(),
-//     AdminTagModule,
-//     AdminPostModule,
-//   ],
-//   controllers: [UserController],
-//   providers: [UserService],
-// })
-// export class AppModule {}
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/auth/Models/users.module';
