1471de5f11f1894767a62d434b613c021c5447b7
jaxin007 <swatdimonbil@gmail.com>
2021-12-28 10:17:58 +0200

    Merge branch 'dev' into feat/add_posts_crud
    # Conflicts:
    #	src/modules/post/dto/createPost.dto.ts
    #	src/modules/post/post.controller.ts
    #	src/modules/posts/dto/createOrUpdatePost.dto.ts
    #	src/modules/posts/dto/createPost.dto.ts
    #	src/modules/posts/dto/getPostsListQuery.dto.ts
    #	src/modules/posts/posts.service.ts

---
+++ src/modules/posts/dto/createOrUpdatePost.dto.ts
@@ -0,0 +1,49 @@
+import { IsInt, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsExistInDb } from '../../../validation/isExistInDb';
+
+export class CreateOrUpdatePostDto {
+  @ApiProperty({
+    example: 'posts header',
+    description: 'header for posts',
+    required: true,
+    type: 'string',
+  })
+  @IsString()
+  head: string;
+
+  @ApiProperty({
+    example: 'subtitle example',
+    description: 'posts subtitle',
+    required: true,
+    type: 'string',
+  })
+  @IsString()
+  subtitle: string;
+
+  @ApiProperty({
+    example: 'qwe',
+    description: 'posts excerpt',
+    required: true,
+    type: 'string',
+  })
+  @IsString()
+  excerpt: string;
+
+  @IsInt()
+  @IsExistInDb('file', 'id')
+  imageId: number;
+
+  @IsInt()
+  @IsExistInDb('file', 'id')
+  previewImageId: number;
+
+  @IsInt({ each: true })
+  tagsIds: number[];
+
+  @IsInt()
+  minutes_to_read: number;
+
+  @IsString()
+  body: string;
+}
